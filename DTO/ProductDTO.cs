eae153c65f2dd959b2300d3e9109919db683346c
sheymor21 <josearmando_081@outlook.com>
2022-08-16 23:56:37 -0400

    add products endpoints

---
+++ DTO/ProductDTO.cs
@@ -0,0 +1,11 @@
+namespace DTO
+{
+    public class ProductDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public int Stock { get; set; }
+        public double Price { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
+using dbContext;
+using DTO;
+using Microsoft.EntityFrameworkCore;
+using Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var conectionstring = builder.Configuration.GetConnectionString("sqlite");
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DBContext>((options) => options.UseSqlite(conectionstring));
 
 var app = builder.Build();
 
@@ -16,6 +22,79 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapPost("/Product", async (ProductDTO product, DBContext db) =>
+{
+    Product dbProduct = new(product);
+    await db.AddAsync(dbProduct);
+    await db.SaveChangesAsync();
+    return Results.Ok("Se a creado el producto correctamente");
+});
+
+app.MapGet("/ProductAll", (DBContext db) =>
+{
+    return Results.Ok(db.Products.ToList());
+});
+
+app.MapGet("/ProductId", async (int id, DBContext db) =>
+{
+    var product = await db.Products.Where((w) => w.Id == id).FirstOrDefaultAsync();
+    if (product is null)
+    {
+        return Results.NotFound("No existe dicho producto");
+    }
+    return Results.Ok(product);
+});
+
+app.MapGet("/ProductName", async (string name, DBContext db) =>
+{
+
+    var product = await db.Products.Where((w) => w.Name == name).FirstOrDefaultAsync();
+    if (product is null)
+    {
+        return Results.NotFound("No existe dicho producto");
+    }
+    return Results.Ok(product);
+});
+
+app.MapDelete("/ProductId", async (int id, DBContext db) =>
+{
+    var product = await db.Products.Where((w) => w.Id == id).FirstOrDefaultAsync();
+    if (product is null)
+    {
+        return Results.NotFound("No existe dicho producto");
+    }
+    db.Remove(product);
+    await db.SaveChangesAsync();
+    return Results.Ok("Se elimino correctamente el producto");
+});
+
+app.MapPut("/Product", async (int id, ProductDTO product, DBContext db) =>
+{
+    var dbProduct = await db.Products.Where((w) => w.Id == id).FirstOrDefaultAsync();
+    if (dbProduct is null)
+    {
+        return Results.NotFound("No existe dicho producto");
+    }
+    dbProduct.Name = product.Name;
+    dbProduct.Category = product.Category;
+    dbProduct.Description = product.Description;
+    dbProduct.Price = product.Price;
+    await db.SaveChangesAsync();
+    return Results.Ok("Se actualizo el producto correctamente");
+});
+
+app.MapPut("ProductStock", async (int id, int newStock, DBContext db) =>
+{
+    var dbProduct = await db.Products.Where((w) => w.Id == id).FirstOrDefaultAsync();
+    if (dbProduct is null)
+    {
+        return Results.NotFound("No existe dicho producto");
+    }
+    dbProduct.Stock = newStock;
+    await db.SaveChangesAsync();
+    return Results.Ok("Se a actualizado el stock del producto");
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
@@ -23,3 +102,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
